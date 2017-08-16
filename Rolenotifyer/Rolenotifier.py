98d4d3df4cdd57dc96b32c0543a49b86f3a6096f
The-Tasty-Jaffa <The-Tasty-Jaffa@users.noreply.github.com>
2017-08-16 10:32:36 +0100

    Update Rolenotifier.py

--- Rolenotifyer/Rolenotifier.py
+++ Rolenotifyer/Rolenotifier.py
@@ -8,7 +8,7 @@ import os
 #programed by The Tasty Jaffa
 #Some help with gramma from Freud (and also testing it making sure it worked)
 
-class Rolenotifier:
+class RoleNotifier:
     def __init__(self, bot):
         self.bot = bot
         self.settings = dataIO.load_json("data/Tasty/AutoRoleDM/settings.json")
@@ -104,7 +104,7 @@ def check_files(): #Creates json files in the folder
 def setup(bot):
     check_folders()
     check_files()
-    n = Rolenotifier(bot)
+    n = RoleNotifier(bot)
     bot.add_listener(n.Role_Update_check, 'on_member_update')
     bot.add_listener(n.server_join, "on_server_join")
     bot.add_cog(n)
