7cee2496941075f16027ab8f0e50deb8b2ea3399
The-Tasty-Jaffa <The-Tasty-Jaffa@users.noreply.github.com>
2017-10-28 17:24:36 +0100

    Updated rewrite of say cog [debuged]
    Fully working code with major issues fixed

--- say/say.py
+++ say/say.py
@@ -7,6 +7,13 @@ from .utils.chat_formatting import pagify, box
 #The Tasty Jaffa
 #Requested by Freud
 
+def get_role(ctx, role_id):
+    roles = set(ctx.message.server.roles)
+    for role in roles:
+        if role.id == role_id:
+            return role
+    return None
+
 class say:
     def __init__(self, bot):
         self.bot = bot
@@ -28,11 +35,11 @@ class say:
     async def sayset(self, ctx):
         """The 'Say' command set
 
-add - Adds a user to have the abillity to use the say command
+add - Adds a user to have the abillity to use the speak command
 list - list users allowed and permited role
-remove - Removes a user to have the abillity to use the say command"""
+remove - Removes a user to have the abillity to use the speak command"""
         if ctx.invoked_subcommand is None:
-            await self.bot.send_message(ctx.message.channel, "```Please use the say command with: \n add - Adds a **user** to have the abillity to use the say command \n remove - Removes a **user** to have the abillity to use the say command \n role - Adds a role and those with it can use the speak command \n list - lists permited users and the permited role```")
+            await self.bot.send_message(ctx.message.channel, "```Please use the speak command with: \n add - Adds a **user** to have the abillity to use the speak command \n remove - Removes a **user** to have the abillity to use the speak command \n role - Adds a role and those with it can use the speak command \n list - lists permited users and the permited role```")
 
     @sayset.command(name="list", pass_context=True)
     @checks.admin_or_permissions()
@@ -48,12 +55,15 @@ remove - Removes a user to have the abillity to use the say command"""
         for page in pagify(msg, [" "], shorten_by=16):
             await self.bot.say(box(page.lstrip(" "), lang="diff"))
 
-        await self.bot.send_message(ctx.message.channel, "Permited Role: **{}**".format(self.settings[ctx.message.server.id]["ROLE"]))
-        
+        #gets the name of the role and displays it
+        if self.settings[ctx.message.server.id]["ROLE"] is not None:
+            await self.bot.send_message(ctx.message.channel, "Permited Role: **{}**".format(get_role(ctx, self.settings[ctx.message.server.id]["ROLE"]).name))
+        else:
+            await self.bot.send_message(ctx.message.channel, "No role is permitted")
     @sayset.command(name="add", pass_context=True, no_pm=True)
     @checks.admin_or_permissions()
     async def say_add (self, ctx, user: discord.Member):
-        """Adds a [user] to have the abillity to use the say command"""
+        """Adds a [user] to have the abillity to use the speak command"""
         self.settings[ctx.message.server.id]["USERS"].append(user.id)
         self.save()
         await self.bot.send_message(ctx.message.channel, "Done!")
@@ -61,7 +71,7 @@ remove - Removes a user to have the abillity to use the say command"""
     @sayset.command(name="remove", pass_context=True, no_pm=True)
     @checks.admin_or_permissions()
     async def say_remove (self, ctx, user: discord.Member):
-        """Removes a [user] to have the abillity to use the say command"""
+        """Removes a [user] to have the abillity to use the speak command"""
         try:
             self.settings[ctx.message.server.id]["USERS"].remove(user.id)
             self.save()
@@ -73,14 +83,26 @@ remove - Removes a user to have the abillity to use the say command"""
     @sayset.command(name="role", pass_context=True)
     @checks.admin_or_permissions()
     async def say_role(self, ctx, role_name:str):
-        self.settings[ctx.message.server.id]["ROLE"]=role_name
+        role = discord.utils.get(ctx.message.server.roles, name=role_name)
+
+        if role is not None:
+            self.settings[ctx.message.server.id]["ROLE"] = role.id
+            self.save()
+            await self.bot.send_message(ctx.message.channel, "Role added!")
+
+        else:
+            await self.bot.send_message(ctx.message.channel, "Role not found!")
+
+    @sayset.command(name="mentions", pass_context=True)
+    @checks.admin_or_permissions()
+    async def say_max_mentions(self, ctx, max_mentions:int):
+        self.settings[ctx.message.server.id]["MAX_MENTIONS"]=max_mentions
         self.save()
-        await self.bot.send_message(ctx.message.channel, "Role {} added".format(role_name))
-            
+        await self.bot.send_message(ctx.message.channel, "Done!")
 
     @commands.command(name="speak", pass_context=True)
     async def bot_say(self, ctx, *, text):
-        """The bot says what you tell it to"""
+        """The bot speaks what you tell it to"""
 
         if '@everyone' in ctx.message.content and '@here' in ctx.message.content and len(ctx.message.mentions) > self.settings[ctx.message.server.id]["MAX_MENTIONS"]:
             await self.bot.send_message(ctx.message.channel, "Woh! {}, please don't do that".format(ctx.message.author.mention))
@@ -88,8 +110,9 @@ remove - Removes a user to have the abillity to use the say command"""
     
         #IF there are no mentions such as @everyone or @here must test useing a string
         
-        if ctx.message.channel.permissions_for(ctx.message.server.me).manage_messages:         
-            if ctx.message.author.id in self.settings[ctx.message.server.id]["USERS"] or self.settings[ctx.message.server.id]["ROLE"] in ctx.message.author.roles.names:
+        if ctx.message.channel.permissions_for(ctx.message.server.me).manage_messages:
+            #checks if they are allowed (role or permitted)         
+            if ctx.message.author.id in self.settings[ctx.message.server.id]["USERS"] or get_role(ctx, self.settings[ctx.message.server.id]["ROLE"])  in ctx.message.author.roles:
                 await self.bot.delete_message(ctx.message)
                 await self.bot.send_message(ctx.message.channel, text)
             else: 
@@ -97,7 +120,8 @@ remove - Removes a user to have the abillity to use the say command"""
         else: 
             await self.bot.say("This command requires the **Manage Messages** permission.")
 
-    async def save(self):
+
+    def save(self):
         dataIO.save_json("data/Tasty/say/settings.json", self.settings)
 
     async def server_join(self, server):
@@ -126,4 +150,5 @@ def setup(bot):
     n = say(bot)
     bot.add_listener(n.server_join, "on_server_join")
     bot.add_cog(n)
+
 
