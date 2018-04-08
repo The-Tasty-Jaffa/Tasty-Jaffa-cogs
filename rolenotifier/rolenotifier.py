dfc63d25495cbc1b58b8693a799f68db85617ee3
The-Tasty-Jaffa <The-Tasty-Jaffa@users.noreply.github.com>
2018-04-08 15:00:26 +0100

    Update rolenotifier.py

--- rolenotifier/rolenotifier.py
+++ rolenotifier/rolenotifier.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 from discord import Embed
+from discord import Forbidden
 from .utils import checks
 from .utils.dataIO import dataIO
 import os
@@ -18,7 +19,6 @@ class RoleNotifier:
         for x in self.bot.servers:
             try:
                 #to avoid breaking things, checks if it is in correct format
-                #NOTE: Only works when reloaded!
                 items = self.settings[x.id]
 
                 for key,value in items.items():
@@ -28,14 +28,33 @@ class RoleNotifier:
                     self.settings[x.id][key] = {'MSG':value, 'CHANNEL':None}                   
 
             except Exception as e:
-                print(e)
                 self.settings[x.id]={
                     }
 
         dataIO.save_json("data/Tasty/AutoRoleDM/settings.json", self.settings)
-    
+
+    #Data validity checker (if joined server when bot was down)
+    async def data_check(self): #When servers are aviable
+        for x in self.bot.servers:
+            try:
+                #to avoid breaking things, checks if it is in correct format
+                items = self.settings[x.id]
+
+                for key,value in items.items():
+                    if type(value) == type(dict()):
+                        continue
+
+                    self.settings[x.id][key] = {'MSG':value, 'CHANNEL':None}                   
+
+            except Exception as e:
+                self.settings[x.id]={
+                    }
+
+        dataIO.save_json("data/Tasty/AutoRoleDM/settings.json", self.settings)
+
     #Checks if it needs to send a notification
     async def Role_Update_check(self, before, after):
+
         check_roles = [r for r in after.roles if r not in before.roles]
         for role in check_roles:
             if role.name in self.settings[after.server.id]:
@@ -47,27 +66,34 @@ class RoleNotifier:
                 else:
                     channel = after
 
-                msg = self.settings[after.server.id][role.name]['MSG']
-                await self.bot.send_message(channel, msg.format(role.name, after.server.name, after.mention, after.name))
+                try:
+                    msg = self.settings[after.server.id][role.name]['MSG']
+                    await self.bot.send_message(channel, msg.format(role.name, after.server.name, after.mention, after.name))
+                
+                except Forbidden:
+                    await self.bot.send_message(after.server.owner, "I could not send a message to {0}. \n I either cannot send messsage to the channel or the user does not accept Direct messages.".format(channel.name))
 
     #General spesific help with the cog
-    @commands.group(pass_context=True, name="setnotifiroles")
+    @commands.group(pass_context=True, name="setroles")
     @checks.admin_or_permissions(manage_roles=True)
-    async def Notification_message_syntax_on_Tasty_Jaffa_cogs(self, ctx): # yes long name :)
+    async def set_notification_roles(self, ctx): 
         """Command group: Use with no sub command for infomation on the syntax on how to use this cog"""
-        em = Embed(tile="Infomation on how to use the notification system, use `[p]help setnotifiroles` to find right sub commands comands")
-        em.add_field(name="Use of `{0}`", value = "The name of the role that was gained", inline=True)
-        em.add_field(name="Use of `{1}`", value = "The name of the server that the role was gained in", inline=True)
-        em.add_field(name="Use of `{2}`", value = "Mentions the user who gained the role", inline=True)
-        em.add_field(name="Use of `{3}`", value = "The name of the user who gained the role", inline=True)
-        em.add_field(name="How to enter the msg", value='example `[p]setnotifiroles add Member Well done {2}! You have gained {0}!`', inline=False)
-        em.set_footer(text="This cog can be found here - https://github.com/The-Tasty-Jaffa/Tasty-Jaffa-cogs/")
-        await self.bot.send_message(ctx.message.channel, embed=em)
+        
+        if ctx.invoked_subcommand is None:
+            em = Embed(tile="Infomation on how to use the notification system, use `[p]help setnotifiroles` to find right sub commands comands")
+            em.add_field(name="Use of `{0}`", value = "The name of the role that was gained", inline=False)
+            em.add_field(name="Use of `{1}`", value = "The name of the server that the role was gained in", inline=False)
+            em.add_field(name="Use of `{2}`", value = "Mentions the user who gained the role", inline=False)
+            em.add_field(name="Use of `{3}`", value = "The name of the user who gained the role", inline=False)
+            em.add_field(name="How to enter the msg", value='example `[p]setroles add Member Well done {2}! You have gained {0}!`', inline=False)
+            em.add_field(name="Subcommands", value='Use `[p]help setroles` to find out all the subcommands and what they do!', inline=False)
+            em.set_footer(text="This cog can be found here - https://github.com/The-Tasty-Jaffa/Tasty-Jaffa-cogs/")
+            await self.bot.send_message(ctx.message.channel, embed=em)
     
-    @setnotifiroles.command(pass_context=True, name="add")
+    @set_notification_roles.command(pass_context=True, name="add")
     @checks.admin_or_permissions(manage_roles=True)
     async def set_roles(self, ctx, role_name:str, *, msg:str="Well done {2}! In {1} you have just gained {0} role"):
-        """For infomation on this command use [p]setnotifiroles with no sub co, use speech marks for the [msg] paramater"""
+        """Adds an alert for a role - For infomation on this command use `[p]setroles`"""
 
         #Default settings
         channel = None
@@ -107,7 +133,7 @@ class RoleNotifier:
         else:
             await self.bot.send_message(ctx.message.channel, "Aborted! -- Make sure to have a look at the documention on the git repo page!")
 
-    @setnotifiroles.command(pass_context=True, name="list")
+    @set_notification_roles.command(pass_context=True, name="list")
     @checks.admin_or_permissions(manage_roles=True)
     async def list_roles(self, ctx):
         """Lists all the roles that have been given/set a notification for in this server."""
@@ -123,15 +149,13 @@ class RoleNotifier:
         em.set_footer(text="This cog can be found here - https://github.com/The-Tasty-Jaffa/Tasty-Jaffa-cogs/")
         await self.bot.send_message(ctx.message.channel, embed=em)
 
-    @setnotifiroles.command(name="remove", pass_context=True)
+    @set_notification_roles.command(name="remove", pass_context=True)
     @checks.admin_or_permissions(manage_roles=True)
     async def remove_roles(self, ctx, role_name):
         """Removes roles from notifications, use `[p]listroles` to find out what roles are set to be notifified"""
         await self.bot.send_message(ctx.message.channel, "This will remove the `{}` role and people who gain this role will no longer be notified. \n\n__are you sure you want this? **y/n**__".format(role_name))
         response = await self.bot.wait_for_message(channel = ctx.message.channel, author = ctx.message.author)
-
         if 'y' in response.lower():
-
             try:
                 self.settings[ctx.message.server.id].pop(role_name)
                 dataIO.save_json("data/Tasty/AutoRoleDM/settings.json", self.settings)
@@ -148,12 +172,12 @@ class RoleNotifier:
     async def server_leave(self,server):
         self.settings.remove(server.id)
 
-def check_folders(): #Creates a folder
+def check_folders(): #Creates a folder, if it doesn't exist
     if not os.path.exists("data/Tasty/AutoRoleDM"):
         print("Creating data/Tasty/AutoRoleDM folder...")
         os.makedirs("data/Tasty/AutoRoleDM")
 
-def check_files(): #Creates json files in the folder
+def check_files(): #Creates json files in the folder, if it doesn't exist
     if not dataIO.is_valid_json("data/Tasty/AutoRoleDM/settings.json"):
         print("Creating empty settings.json...")    
         dataIO.save_json("data/Tasty/AutoRoleDM/settings.json", {})
@@ -170,5 +194,7 @@ def setup(bot):
     bot.add_listener(this_cog.Role_Update_check, 'on_member_update')
     bot.add_listener(this_cog.server_join, "on_server_join")
     bot.add_listener(this_cog.server_leave, "on_server_remove")
+    bot.add_listener(this_cog.data_check, "on_ready")
+
     bot.add_cog(this_cog)
 
