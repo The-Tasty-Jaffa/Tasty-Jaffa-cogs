9b8a16b73886c5427ac56579648bc54f3f42cfd2
The-Tasty-Jaffa <20139654+The-Tasty-Jaffa@users.noreply.github.com>
2024-03-16 21:10:44 +0000

    Fix problem that prevents temp voice from being loaded

--- tempvoice/__init__.py
+++ tempvoice/__init__.py
@@ -4,14 +4,14 @@ import asyncio
 from .tempvoice import TempVoice
 
 
-def setup(bot):
+async def setup(bot):
     temp_core = TempVoice(bot)
 
     loop = asyncio.get_event_loop()
-    loop.create_task(temp_core.voice_check())
+    await loop.create_task(temp_core.voice_check())
 
     # Listeners
     # Thankyou to Tobotimus for giving a simple example on listeners
     bot.add_listener(temp_core.auto_voice, "on_voice_state_update")
 
-    bot.add_cog(temp_core)
+    await bot.add_cog(temp_core)
